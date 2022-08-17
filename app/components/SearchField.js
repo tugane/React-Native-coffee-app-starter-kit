92b1bb7822d992a08fe0f4fc66e60463390c1732
tugane <tooamiel@gmail.com>
2022-08-17 14:35:06 +0200

    setup env

---
+++ app/components/SearchField.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SearchField = () => {
+  return (
+    <View>
+      <Text>SearchField</Text>
+    </View>
+  );
+};
+
+export default SearchField;
+
+const styles = StyleSheet.create({});
---
+++ app/screens/CoffeeDetailsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CoffeeDetailsScreen = () => {
+  return (
+    <View>
+      <Text>CoffeeDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default CoffeeDetailsScreen;
+
+const styles = StyleSheet.create({});
